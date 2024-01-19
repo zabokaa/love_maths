36b28879ad18d1449733ffcbbda76a4ee13dec71
zabokaa <sab_fem@gmx.de>
2024-01-19 17:46:01 +0100

    add event listener for buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,27 @@
+
+// wait for DOM to finish loading before running the game 
+document.addEventListener("DOMContentLoaded", function() {
+    // event listeners
+    // getting all buttons as an array:
+    let buttons = document.getElementsByTagName("button");
+// iterate through array
+    // for (let i = 0, i < buttons.length, i++)
+    // MORE MODERN ALTERNATIVE:
+    for (let button of buttons) {
+        // button now is individual button event 
+        button.addEventListener("click", function() {
+            // this = referring to specific button that is clicked and checking the data-type of it
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);  //backquote !!
+            }
+        })
+    }
+})
+// event handler
+
 function runGame() {
 
 }
